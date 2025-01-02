8c3564e45346366b1b533e1907a04a7f18652517
Artem Osta <ostart@mail.ru>
2025-01-02 08:08:59 +0300

    Refactor for localhost. K8s commented

--- Microservices in .NET/api-gateway/ApiGateway/obj/Debug/net9.0/ApiGateway.AssemblyInfo.cs
+++ Microservices in .NET/api-gateway/ApiGateway/obj/Debug/net9.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cf745f2d38a7b3a2a4a3202251709d05b29119c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
